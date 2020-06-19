3ddd4954d02647624d79d028505915b0e8ede643
GlosuU <glosuu31@gmail.com>
2020-06-19 17:52:03 +0200

    (1.0.1) Added simple functionality and a test.html page

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-const express = require('express');
+const express = require('express')
+    , http = require('http')
+    , path = require('path')
+
 var app = express();
+
+// CONFIGURATION
+app.set('port', process.env.PORT || 3000);
+
+// MIDDLEWARES
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.configure(() => {
+  // app.set('views', path.join(__dirname, 'views'));
+  // app.set('view engine', 'ejs');
+  // app.use(express.favicon());
+  // app.use(express.logger('dev'));
+  // app.use(express.bodyParser());
+  // app.use(express.methodOverride());
+  // app.use(express.cookieParser('your secret here'));
+  // app.use(express.session());
+  // app.use(app.router);
+// });
+
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.send('Hello World Express');
 });
-app.listen(process.env.PORT || 3000)
+
+// CREATE HTTP SERVER and LISTEN
+http.createServer(app).listen(app.get('port'), () => {
+  console.log("Express server listening on port " + app.get('port'));
+});

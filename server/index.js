232153747f0472b910790a679de19c527ade31db
GlosuU <glosuu31@gmail.com>
2020-09-25 20:30:47 +0200

    Relocated dist. Cleanup

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ app.use("/api/export", exporter);
 // Handle production
 if (process.env.NODE_ENV === "production") {
 	// Static directory
-	app.use(express.static(path.resolve(__dirname, "../client/dist")));
+	app.use(express.static(path.resolve(__dirname, "./public/dist")));
 
 	// Handle SPA
-	app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, "../client/dist/index.html")));
+	app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, "./public/dist/index.html")));
 }
 
 const port = process.env.PORT || 3000;
